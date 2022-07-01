6b85ad36ce9fddf22b6c0be9a68f9ab759e14a70
crushon9 <catco929@naver.com>
2022-07-01 13:45:21 +0900

    fix

--- JavaScript_color button.js
+++ JavaScript_color button.js
@@ -1,8 +1,8 @@
 var Links = {
-  SetColor: function (color){
+  SetColor: function (color) {
     var alist = document.querySelectorAll('.gridleft ul li, .gridleft ol li, h1');
     var i = 0;
-    while(i < alist.length){
+    while (i < alist.length) {
       alist[i].style.color = color;
       i = i + 1;
     }
@@ -10,24 +10,25 @@ var Links = {
 }
 
 var Body = {
-  SetColor: function (color){
-    document.querySelector('body').style.color=color;
+  SetColor: function (color) {
+    document.querySelector('body').style.color = color;
   },
-  SetBackgroundColor: function (color){document.querySelector('body').style.backgroundColor=color;
+  SetBackgroundColor: function (color) {
+    document.querySelector('body').style.backgroundColor = color;
   }
 }
 //
-function 함수버튼2 (self) {
+function 함수버튼2(self) {
   var target = document.querySelector('body');
-    if(self.value==='Night로 전환'){
-      Body.SetBackgroundColor('black');
-      Body.SetColor('white');
-      self.value='Day로 전환';
-      Links.SetColor('powderblue');
+  if (self.value === 'Night로 전환') {
+    Body.SetBackgroundColor('black');
+    Body.SetColor('white');
+    self.value = 'Day로 전환';
+    Links.SetColor('powderblue');
   } else {
-      Body.SetBackgroundColor('white');
-      Body.SetColor('black');
-      self.value='Night로 전환';
-      Links.SetColor('blue');
+    Body.SetBackgroundColor('white');
+    Body.SetColor('black');
+    self.value = 'Night로 전환';
+    Links.SetColor('blue');
   }
 }
